877f27bcf64d7b9a46e0aba73d52f4345f2781e9
OJWatson <oj.watson@hotmail.co.uk>
2020-11-16 09:07:50 +0000

    test for removing non numerics from list

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -6,3 +6,14 @@ test_that("null-or-value works", {
   expect_equal(NULL %||% NULL, NULL)
   expect_equal(NULL %||% 2, 2)
 })
+
+test_that("remove_non_numerics works", {
+  l <- list(
+    "a" = 2,
+    "b" = "a",
+    "c" = 2.1,
+    "d" = array(0, c(2, 2, 1))
+  )
+  l <- remove_non_numerics(l)
+  expect_named(l, c("a", "c", "d"))
+})
