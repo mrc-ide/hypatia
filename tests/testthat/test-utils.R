4d417de9c61f335870b8178afcd4b458b87fefbe
OJWatson <oj.watson@hotmail.co.uk>
2020-12-07 00:14:09 +0000

    vcapply and codecov is back and ready to go

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -7,6 +7,13 @@ test_that("null-or-value works", {
   expect_equal(NULL %||% 2, 2)
 })
 
+test_that("vcapply works") {
+
+  expect_length(vcapply(1:5, function(x) {letters[x]}), 5)
+  expect_equal(vcapply(1:5, function(x) {letters[x]}), letters[1:5])
+
+}
+
 test_that("r_exp works", {
 
   ret <- r_exp(1, 0.1)
@@ -37,3 +44,4 @@ test_that("bernoulli_multi_p works on the boundary", {
   mockery::stub(bernoulli_multi_p, 'runif', mockery::mock(c(.1, .7)))
   expect_equal(bernoulli_multi_p(c(.2, .7)), c(TRUE, FALSE))
 })
+
